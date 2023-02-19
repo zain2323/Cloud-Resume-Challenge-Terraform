4d22d6f13cdbc1e6a5037281d4cdcf3b64e906b6
Zain Siddiqui <zainsiddiqui2323@gmail.com>
2023-02-19 19:28:53 +0500

    added code for provisioning lambda

--- main.tf
+++ main.tf
@@ -85,6 +85,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 }
 
+# While creating dynamodb only specify the key attributes and create non-key attributes using application
 resource "aws_dynamodb_table" "basic-dynamodb-table" {
   name           = "my-table"
   billing_mode   = "PAY_PER_REQUEST"
@@ -99,4 +100,40 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
     Name        = "dynamodb-table-1"
     Environment = "production"
   }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = "lambda_function.zip"
+  function_name = "lambda_demo"
+  role          = aws_iam_role.iam_for_lambda.arn
+
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+  source_code_hash = filebase64sha256("lambda_function.zip")
+
+  runtime = "python3.9"
+  handler       = "lambda_handler.test"
 }
